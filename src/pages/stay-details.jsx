55af74d070474315620ae6899c11a97c782a7156
ori08 <orielgrabli@gmail.com>
2022-09-16 17:45:48 +0300

    Add reserve modal

--- src/pages/stay-details.jsx
+++ src/pages/stay-details.jsx
@@ -10,7 +10,7 @@ import { PlaceOffer } from '../cmps/stays/stay-details-reserve'
 import { RoomInfo } from '../cmps/stays/stay-details-title'
 import { RoomImages } from '../cmps/stays/stay-details-img'
 import { RoomDatails } from '../cmps/stays/stay-details-secondary'
-
+import { ReserveModal } from '../cmps/modal/modal-reserve'
 
 
 export const StayDetails = () => {
@@ -25,17 +25,18 @@ export const StayDetails = () => {
     const loadStay = () => {
         const stayId = params.id
         stayService.getById(stayId).then(stay => {
-            console.log("return stay",stay);
+            console.log("return stay", stay);
             setStay(stay)
         })
     }
 
-if(!stay)return
+    if (!stay) return
 
 
     return (
         <div className='room'>
             <RoomInfo stay={stay} />
+            <ReserveModal stay={stay} />
             <RoomImages stay={stay} />
             <RoomDatails stay={stay} />
             <PlaceOffer stay={stay} />
