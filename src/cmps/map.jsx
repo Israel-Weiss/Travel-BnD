50303a007e2a98215e4e561ed3ee4a91e93a3d1c
Chen <chenattias19@gmail.com>
2022-09-21 02:46:54 +0300

    add map basic cmp

--- src/cmps/map.jsx
+++ src/cmps/map.jsx
@@ -6,9 +6,9 @@ import { FaHome } from 'react-icons/fa';
 const AnyReactComponent = ({ text }) => <div className='outer-layer'><div className="map-icon">{text}</div> </div>
 
 export function GoogleMap({ stay }) {    
-    const lng = parseFloat(stay.loc.lng)
-    const lat = parseFloat(stay.loc.lat)
-    const [coordinates, setCoordinates] = useState({ lat: lng, lng: lat })
+    const lng = parseFloat(stay.loc.lat)
+    const lat = parseFloat(stay.loc.lng)
+    const [coordinates, setCoordinates] = useState({ lat: lat, lng: lng })
     const zoom = 16
     const mapIcon = <FaHome/>
     const onClick = ({ x, y, lat, lng, event }) => {
@@ -16,10 +16,9 @@ export function GoogleMap({ stay }) {
     }
 
     return (
-        // Important! Always set the container height explicitly
         <div className="google-map" style={{ height: '66.50vh', width: '100%' , padding: '20px 20px 20px 20px'}}>
             <GoogleMapReact
-                onClick={onClick}
+                // onClick={onClick}
                 bootstrapURLKeys={{ key: "AIzaSyBvTjJLgXv_JG78L_VC13fO7vJjnzeBzH8" }}
                 defaultCenter={coordinates}
                 center={coordinates}
