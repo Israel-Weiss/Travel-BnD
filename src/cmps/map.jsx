0cfda79d52ad71ab486b2e54e2d9a91edc547c04
ori08 <orielgrabli@gmail.com>
2022-09-21 12:56:19 +0300

    connect backend

--- src/cmps/map.jsx
+++ src/cmps/map.jsx
@@ -1,36 +1,36 @@
-// import React from 'react'
-// import GoogleMapReact from 'google-map-react' 
-// import { useState } from 'react'
-// import { FaHome } from 'react-icons/fa';
+import React from 'react'
+import GoogleMapReact from 'google-map-react' 
+import { useState } from 'react'
+import { FaHome } from 'react-icons/fa';
 
-// const AnyReactComponent = ({ text }) => <div className='outer-layer'><div className="map-icon">{text}</div> </div>
+const AnyReactComponent = ({ text }) => <div className='outer-layer'><div className="map-icon">{text}</div> </div>
 
-// export function GoogleMap({ stay }) {    
-//     const lng = parseFloat(stay.loc.lat)
-//     const lat = parseFloat(stay.loc.lng)
-//     const [coordinates, setCoordinates] = useState({ lat: lat, lng: lng })
-//     const zoom = 16
-//     const mapIcon = <FaHome/>
-//     const onClick = ({ x, y, lat, lng, event }) => {
-//         setCoordinates({lat, lng})
-//     }
+export function GoogleMap({ stay }) {    
+    const lng = parseFloat(stay.loc.lat)
+    const lat = parseFloat(stay.loc.lng)
+    const [coordinates, setCoordinates] = useState({ lat: lat, lng: lng })
+    const zoom = 16
+    const mapIcon = <FaHome/>
+    const onClick = ({ x, y, lat, lng, event }) => {
+        setCoordinates({lat, lng})
+    }
 
-//     return (
-//         <div className="google-map" style={{ height: '66.50vh', width: '100%' , padding: '20px 20px 20px 20px'}}>
-//             <GoogleMapReact
-//                 // onClick={onClick}
-//                 bootstrapURLKeys={{ key: "AIzaSyBvTjJLgXv_JG78L_VC13fO7vJjnzeBzH8" }}
-//                 defaultCenter={coordinates}
-//                 center={coordinates}
-//                 defaultZoom={zoom}
+    return (
+        <div className="google-map" style={{ height: '66.50vh', width: '100%' , padding: '20px 20px 20px 20px'}}>
+            <GoogleMapReact
+                // onClick={onClick}
+                bootstrapURLKeys={{ key: "AIzaSyBvTjJLgXv_JG78L_VC13fO7vJjnzeBzH8" }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={zoom}
                 
-//             >
-//                 <AnyReactComponent
-//                     lat={coordinates.lat}
-//                     lng={coordinates.lng}
-//                     text={mapIcon}
-//                 />
-//             </GoogleMapReact>
-//         </div>
-//     ) 
-// }
+            >
+                <AnyReactComponent
+                    lat={coordinates.lat}
+                    lng={coordinates.lng}
+                    text={mapIcon}
+                />
+            </GoogleMapReact>
+        </div>
+    ) 
+}
--- src/services/stay.service.js
+++ src/services/stay.service.js
@@ -19,6 +19,7 @@ import aircondition from '../assets/imgs/house/place-offer/aircondition.svg'
 import crib from '../assets/imgs/house/place-offer/crib.svg'
 
 import { storageService } from "./async-storage.service"
+import { httpService } from './http.service'
 
 export const stayService = {
   getById,
@@ -27,6 +28,9 @@ export const stayService = {
   mapIcon,
   getRandomIntInclusive
 }
+
+const BASE_URL = "stays/"
+
 
 function getById(stayId) {
   let stay = null
@@ -37,19 +41,14 @@ function getById(stayId) {
   return Promise.resolve(stay)
 }
 
-function query(tag = null, text = null) {
-  let stays
+async function query(tag = null, text = null,filterBy) {
+  const stays = await httpService.get(BASE_URL, { params: filterBy })
+
   let stayToDisplay = []
-  if (!stays) {
-    stays = gStays
-    storageService._save('stays', stays)
-  }
-  else stays = storageService.getStays()
 
-stays.map(stay=>{
-  if(!stay.host)console.log(stay._id);
-})
-
+  stays.map(stay => {
+    if (!stay.host) console.log(stay._id);
+  })
 
   if (tag) {
     stayToDisplay = stays.filter(stay => stay.type.includes(tag))
@@ -150,3 +149,41 @@ function getRandomIntInclusive(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
+
+
+
+
+
+// function query(tag = null, text = null) {
+//   let stays
+//   let stayToDisplay = []
+//   if (!stays) {
+//     stays = gStays
+//     storageService._save('stays', stays)
+//   }
+//   else stays = storageService.getStays()
+
+//   stays.map(stay => {
+//     if (!stay.host) console.log(stay._id);
+//   })
+
+
+//   if (tag) {
+//     stayToDisplay = stays.filter(stay => stay.type.includes(tag))
+//     return Promise.resolve(stayToDisplay)
+//   }
+//   else if (text) {
+//     const lowerText = text.toLowerCase()
+//     stayToDisplay = stays.filter(stay =>
+//       (stay.name.toLowerCase().includes(lowerText))
+//       || (stay.loc.country.toLowerCase().includes(lowerText))
+//       || (stay.loc.city.toLowerCase().includes(lowerText)))
+//     console.log(stayToDisplay);
+//     return Promise.resolve(stayToDisplay)
+//   }
+//   else {
+//     storageService._save('stays', stays)
+//     if (gStays.length > 100) stayToDisplay = gStays.splice(0, 100)
+//     return Promise.resolve(stayToDisplay)
+//   }
+// }
